74d2582d6ae6df8ebfcc6899e607fd160ad4c485
Sabrina Santos da Silva <sabrina.silva@dbserver.com.br>
2019-08-16 15:19:22 -0300

    pipe

---
+++ src/app/photos/photo-list/filter-by-descripition.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Photo } from '../photo/photo';
+import { PhotoComponent } from '../photo/photo.component';
+
+
+@Pipe({name: 'filterByDescripition'})
+export class FilterByDescripition implements PipeTransform {
+
+  transform(photos: Photo[], descriptionQuery: string) {
+    descriptionQuery = descriptionQuery.trim().toLowerCase();
+
+    if (descriptionQuery) {
+      return photos.filter(photo => photo.description )
+    } else {
+      return photos;
+    }
+  }
+
+
+}
